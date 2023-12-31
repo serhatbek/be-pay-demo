ad77de652c2f1fcc728becef805cc556beaa6cd8
serhatbek <serhat.bekk@gmail.com>
2023-12-31 19:20:12 +0300

    edit nav items data

--- src/assets/data/header.js
+++ src/assets/data/header.js
@@ -1,22 +1,31 @@
-headerNav = [
-  {
-    title: 'Home',
+import logo from '../../../public/images/logo.png';
+
+export const headerItems = {
+  logo: logo,
+  nav: [
+    {
+      title: 'Home',
+      link: '/',
+    },
+    {
+      title: 'About',
+      link: '/',
+    },
+    {
+      title: 'Features',
+      link: '/',
+    },
+    {
+      title: 'Pricing List',
+      link: '/',
+    },
+    {
+      title: 'Contact',
+      link: '/',
+    },
+  ],
+  btn: {
+    text: 'Buy Now',
     link: '/',
   },
-  {
-    title: 'About',
-    link: '/',
-  },
-  {
-    title: 'Features',
-    link: '/',
-  },
-  {
-    title: 'Pricing List',
-    link: '/',
-  },
-  {
-    title: 'Contact',
-    link: '/',
-  },
-];
+};
