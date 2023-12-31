5fba4ba9a0adcb40e269a6368948e5258056004b
serhatbek <serhat.bekk@gmail.com>
2023-12-31 17:23:36 +0300

    add header nav items data

---
+++ src/assets/data/header.js
@@ -0,0 +1,22 @@
+headerNav = [
+  {
+    title: 'Home',
+    link: '/',
+  },
+  {
+    title: 'About',
+    link: '/',
+  },
+  {
+    title: 'Features',
+    link: '/',
+  },
+  {
+    title: 'Pricing List',
+    link: '/',
+  },
+  {
+    title: 'Contact',
+    link: '/',
+  },
+];
