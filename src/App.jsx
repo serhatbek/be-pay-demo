d4d1540b7dd88ee0fa494bb1d9cbac5fcc5fec8d
serhatbek <serhat.bekk@gmail.com>
2023-12-31 17:16:09 +0300

    add icons

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,28 @@ function App() {
   return (
     <>
       <h1>
-        Hello <i className='icon icon-pdf-file'></i>
+        <i className='icon icon-lock'></i>
+        <i className='icon icon-menu-close'></i>
+        <i className='icon icon-menu-open'></i>
+        <i className='icon icon-info'></i>
+        <i className='icon icon-web'></i>
+        <i className='icon icon-pay'></i>
+        <i className='icon icon-area'></i>
+        <i className='icon icon-stats'></i>
+        <i className='icon icon-flex'></i>
+        <i className='icon icon-watch'></i>
+        <i className='icon icon-hor-lines'></i>
+        <i className='icon icon-arrow-circle'></i>
+        <i className='icon icon-arrow-left'></i>
+        <i className='icon icon-arrow-right'></i>
+        <i className='icon icon-chevron-up-circle'></i>
+        <i className='icon icon-cart'></i>
+        <i className='icon icon-phone'></i>
+        <i className='icon icon-mail'></i>
+        <i className='icon icon-address'></i>
+        <i className='icon icon-check-circle'></i>
+        <i className='icon icon-facebook'></i>
+        <i className='icon icon-twitter'></i>
       </h1>
     </>
   );
