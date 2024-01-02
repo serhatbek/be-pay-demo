85b6ab2d01cf78118912f907ca7ecaeb29b4f2b4
serhatbek <serhat.bekk@gmail.com>
2024-01-02 22:35:11 +0300

    add footer data

---
+++ src/assets/data/footer.js
@@ -0,0 +1,32 @@
+export const footerItems = {
+  muffin: {
+    url: 'https://muffingroup.com/',
+    text: 'Muffin group',
+  },
+  wordpress: {
+    url: 'WordPress',
+    text: 'https://wordpress.org/',
+  },
+  navItems: [
+    {
+      title: 'Home',
+      link: '/be-pay-demo/',
+    },
+    {
+      title: 'About',
+      link: '/be-pay-demo/about',
+    },
+    {
+      title: 'Features',
+      link: '/be-pay-demo/features',
+    },
+    {
+      title: 'Pricing List',
+      link: '/be-pay-demo/pricing',
+    },
+    {
+      title: 'Contact',
+      link: '/be-pay-demo/contact',
+    },
+  ],
+};
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,6 @@ import { nanoid } from 'nanoid';
 import { Button, Icon } from '../../components';
 import { useState } from 'react';
 import classNames from 'classnames';
-import { CSSTransition } from 'react-transition-group';
 
 const Header = () => {
   const [menuStatus, setMenuStatus] = useState(false);
@@ -27,7 +26,6 @@ const Header = () => {
         </figure>
 
         <Icon size='32' icon='menu-open' onClick={handleMenuOpen} />
-        {/* <CSSTransition></CSSTransition> */}
         <nav className={classNames(`${menuStatus === true ? 'visible' : ''}`)}>
           <Icon size='32' icon='menu-close' onClick={handleMenuClose} />
           <ul>
