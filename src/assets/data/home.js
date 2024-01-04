43cd858d90a0c3b3b1f155f4d82acfc6b8450f59
serhatbek <serhat.bekk@gmail.com>
2024-01-04 13:20:43 +0300

    add all info cards to home page

---
+++ src/assets/data/home.js
@@ -0,0 +1,40 @@
+export const homeItems = {
+  infoItems: [
+    {
+      title: '01 —',
+      text: 'Admin panel',
+      icon: 'web',
+      iconSize: '50',
+    },
+    {
+      title: '02 —',
+      text: 'Safety',
+      icon: 'lock',
+      iconSize: '50',
+    },
+    {
+      title: '03 —',
+      text: 'Statistics',
+      icon: 'stats',
+      iconSize: '50',
+    },
+    {
+      title: '04 —',
+      text: 'Paying',
+      icon: 'pay',
+      iconSize: '50',
+    },
+    {
+      title: '05 —',
+      text: 'Flexible',
+      icon: 'flex',
+      iconSize: '50',
+    },
+    {
+      title: '06 —',
+      text: 'Areas',
+      icon: 'area',
+      iconSize: '50',
+    },
+  ],
+};
