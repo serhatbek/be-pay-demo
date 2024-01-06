3078c6231d62f6edcb9a017c969e0a8f3b7b6a08
serhatbek <serhat.bekk@gmail.com>
2024-01-06 23:09:12 +0300

    add social banner to features page

--- src/pages/Features/Features.jsx
+++ src/pages/Features/Features.jsx
@@ -1,12 +1,25 @@
 import './Features.scss';
 import { nanoid } from 'nanoid';
 import featuresItems from '../../assets/data/features';
-import { Button, SectionBanner } from '../../components';
+import {
+  Button,
+  ClientItem,
+  SectionBanner,
+  SectionClients,
+} from '../../components';
+import { Slide } from '../../components/Slider/Slider';
 
 const Features = () => {
-  const { socialBannerItem } = featuresItems;
+  const { socialBannerItem, clientItems } = featuresItems;
   return (
     <>
+      <SectionClients>
+        {clientItems?.map((item) => (
+          <Slide key={nanoid()}>
+            <ClientItem image={item} />
+          </Slide>
+        ))}
+      </SectionClients>
       <SectionBanner
         title={socialBannerItem.title}
         desc={socialBannerItem.desc}
