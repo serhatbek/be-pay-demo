b5f6e8fc935c777222deb029890c9bdc467c9a22
serhatbek <serhat.bekk@gmail.com>
2024-01-06 23:06:58 +0300

    add clients slider to about page

--- src/assets/data/about.js
+++ src/assets/data/about.js
@@ -1,4 +1,12 @@
+import client1 from '/images/Clients/client-1.png';
+import client2 from '/images/Clients/client-2.png';
+import client3 from '/images/Clients/client-3.png';
+import client4 from '/images/Clients/client-4.png';
+import client5 from '/images/Clients/client-5.png';
+import client6 from '/images/Clients/client-6.png';
+
 const aboutItems = {
+  clientItems: [client1, client2, client3, client4, client5, client6],
   socialBannerItem: {
     title: 'Follow us on social media',
     desc: 'Vestibulum dapibus, mauris nec malesuada fames ac turpis velit',
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,25 @@
 import './About.scss';
 import aboutItems from '../../assets/data/about';
-import { Button, SectionBanner } from '../../components';
+import {
+  Button,
+  ClientItem,
+  SectionBanner,
+  SectionClients,
+} from '../../components';
 import { nanoid } from 'nanoid';
+import { Slide } from '../../components/Slider/Slider';
 
 const About = () => {
-  const { socialBannerItem } = aboutItems;
+  const { socialBannerItem, clientItems } = aboutItems;
   return (
     <>
+      <SectionClients>
+        {clientItems?.map((item) => (
+          <Slide key={nanoid()}>
+            <ClientItem image={item} />
+          </Slide>
+        ))}
+      </SectionClients>
       <SectionBanner
         title={socialBannerItem.title}
         desc={socialBannerItem.desc}
