0c68c67ef5a4cc100f99fd11320ecc974872d650
serhatbek <serhat.bekk@gmail.com>
2024-01-06 23:11:34 +0300

    add social banner to pricing page

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -1,24 +1,42 @@
 import './Pricing.scss';
 import { nanoid } from 'nanoid';
 import pricingItems from '../../assets/data/pricing';
-import { Button, SectionBanner } from '../../components';
+import {
+  Button,
+  ClientItem,
+  SectionBanner,
+  SectionClients,
+} from '../../components';
+import { Slide } from '../../components/Slider/Slider';
 
 const Pricing = () => {
-  const { socialBannerItem } = pricingItems;
+  const { socialBannerItem, clientItems } = pricingItems;
   return (
-    <SectionBanner title={socialBannerItem.title} desc={socialBannerItem.desc}>
-      {socialBannerItem.buttons.map((item) => (
-        <Button
-          key={nanoid()}
-          className={item.btnClass}
-          type='link'
-          href={item.btnLink}
-          iconRight={item.btnIcon}
-        >
-          {item.btnText}
-        </Button>
-      ))}
-    </SectionBanner>
+    <>
+      <SectionClients>
+        {clientItems?.map((item) => (
+          <Slide key={nanoid()}>
+            <ClientItem image={item} />
+          </Slide>
+        ))}
+      </SectionClients>
+      <SectionBanner
+        title={socialBannerItem.title}
+        desc={socialBannerItem.desc}
+      >
+        {socialBannerItem.buttons.map((item) => (
+          <Button
+            key={nanoid()}
+            className={item.btnClass}
+            type='link'
+            href={item.btnLink}
+            iconRight={item.btnIcon}
+          >
+            {item.btnText}
+          </Button>
+        ))}
+      </SectionBanner>
+    </>
   );
 };
 
