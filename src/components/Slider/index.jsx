66157679e7d1e8238aa4ea291ef3f0639e42f8b8
serhatbek <serhat.bekk@gmail.com>
2024-01-08 23:48:40 +0300

    add contact info styling

--- src/components/Slider/index.jsx
+++
@@ -1,2 +0,0 @@
-export { default as TestimonialSlider } from './TestimonialSlider/TestimonialSlider';
-export { default as BrandSlider } from './BrandSlider/BrandSlider';
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,6 +4,7 @@ import contactItems from '../../assets/data/contact';
 import {
   Button,
   ClientItem,
+  ContactInfoItem,
   SectionBanner,
   SectionClients,
   SectionPageTitle,
@@ -11,13 +12,32 @@ import {
 import { Slide } from '../../components/Slider/Slider';
 
 const Contact = () => {
-  const { pageTitleItems, socialBannerItem, clientItems } = contactItems;
+  const { pageTitleItems, socialBannerItem, clientItems, detailItems } =
+    contactItems;
   return (
     <section className='section section-contact section--bg'>
       <SectionPageTitle
         title={pageTitleItems.title}
         desc={pageTitleItems.desc}
       />
+
+      <section className='section-contact__details'>
+        <div className='container'>
+          <div className='section-contact__details__left'>
+            <h3>{detailItems.title}</h3>
+            <div className='box'>
+              {detailItems.list?.map((item) => (
+                <ContactInfoItem
+                  key={nanoid()}
+                  title={item.title}
+                  desc={item.desc}
+                  icon={item.icon}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
 
       <SectionClients>
         {clientItems?.map((item) => (
