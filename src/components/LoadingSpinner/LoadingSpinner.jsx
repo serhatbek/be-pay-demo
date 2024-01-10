64ac290a8c9ca4e2df1e4664516bc0b684cf51cd
serhatbek <serhat.bekk@gmail.com>
2024-01-10 13:06:55 +0300

    loading spinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import './LoadingSpinner.scss';
+
+const LoadingSpinner = () => {
+  return (
+    <div className='lds-ripple'>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
