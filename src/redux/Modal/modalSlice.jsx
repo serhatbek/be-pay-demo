8370128cf1f13110527bb9167f6c8450a44216a6
serhatbek <serhat.bekk@gmail.com>
2024-01-17 13:04:59 +0300

    add modal slice reducers

--- src/redux/Modal/modalSlice.jsx
+++ src/redux/Modal/modalSlice.jsx
@@ -7,7 +7,15 @@ const initialState = {
 const modalSlice = createSlice({
   name: 'modal',
   initialState,
-  reducers: {},
+  reducers: {
+    openModal: (state, action) => {
+      state.activeModal = action.payload;
+    },
+    closeModal: (state) => {
+      state.activeModal = null;
+    },
+  },
 });
 
+export const { openModal, closeModal } = modalSlice.actions;
 export default modalSlice.reducer;
