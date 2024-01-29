23d1c31d71123b3ea62b3dc009c5a4a008e21725
serhatbek <serhat.bekk@gmail.com>
2024-01-29 13:19:52 +0300

    when modal is open, fix app-wrapper position

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,17 @@
 import { useSelector } from 'react-redux';
 import FormSubmitModal from './FormSubmitModal/FormSubmitModal';
+import { useEffect } from 'react';
 
-const Modal = () => {
+const Modal = ({ setIsModalOpen }) => {
   const { activeModal } = useSelector((state) => state.Modal);
+
+  useEffect(() => {
+    if (activeModal !== null) {
+      setIsModalOpen(true);
+    } else {
+      setIsModalOpen(false);
+    }
+  }, [activeModal]);
 
   return <>{activeModal === 'form-submit-modal' && <FormSubmitModal />}</>;
 };
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,16 +1,23 @@
 import { Outlet } from 'react-router-dom';
 import { Footer, Header, Modal } from '../../components';
+import { useState } from 'react';
 
 const Layout = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  console.log(isModalOpen);
   return (
-    <>
+    <div
+      className={`${
+        isModalOpen ? 'app-wrapper position-fixed' : 'app-wrapper'
+      }`}
+    >
       <Header />
       <main>
         <Outlet />
       </main>
-      <Modal />
       <Footer />
-    </>
+      <Modal setIsModalOpen={setIsModalOpen} />
+    </div>
   );
 };
 
